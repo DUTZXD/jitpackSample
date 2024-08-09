2f7539de37639550a92460039ccc02f318687be7
zhangxiaodong91 <zhangxiaodong91@jd.com>
2024-08-09 10:12:55 +0800

    1111

--- app/src/main/java/com/zxd/jitpacksample/MainActivity.kt
+++ app/src/main/java/com/zxd/jitpacksample/MainActivity.kt
@@ -1,13 +1,59 @@
 package com.zxd.jitpacksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.zxd.jitlib.CalUtils
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+import com.zxd.composetest.ui.NestCollapsingToolbarLayout
+
 
 class MainActivity : AppCompatActivity() {
+
+    //    private var toolbar: NestCollapsingToolbarLayout? = null
+//
+//    private var coupon: LinearLayout? = null
+//
+//    private var title: TextView? = null
+    private var appBarLayout: AppBarLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println(CalUtils.add(1,2))
+        setContentView(R.layout.zxd_layout)
+        appBarLayout = findViewById(R.id.app_bar)
+        appBarLayout?.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val totalScrollRange = appBarLayout.totalScrollRange
+            val progress = (-verticalOffset).toFloat() / totalScrollRange.toFloat()
+
+            Log.d("@@#", "verticalOffset: $verticalOffset")
+            Log.d("@@#", "totalScrollRange: $totalScrollRange")
+            Log.d("@@#", "progress: $progress")
+
+            // 缩放视图
+//            customTitleView.setScaleX(1f + progress)
+//            customTitleView.setScaleY(1f + progress)
+
+            // 根据需要调整视图的位置
+            // customTitleView.setTranslationY(-verticalOffset);
+        })
+//        toolbar = findViewById(R.id.tool_bar)
+//        coupon = findViewById(R.id.coupon)
+//        title = findViewById(R.id.title_discount)
+//        toolbar?.setOnScrimesShowListener(object :
+//            NestCollapsingToolbarLayout.OnScrimsShowListener {
+//            override fun onScrimsShowChange(
+//                nestCollapsingToolbarLayout: NestCollapsingToolbarLayout,
+//                isScrimesShow: Boolean
+//            ) {
+//                if (isScrimesShow) {
+//                    coupon?.visibility = VISIBLE
+//                } else {
+//                    coupon?.visibility = GONE
+//                    title?.setOnTouchListener(null)
+//                }
+//            }
+//        })
     }
 }
