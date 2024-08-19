a03eea66a019e2c7fb2b56d9557c19885b6410b8
zhangxiaodong91 <zhangxiaodong91@jd.com>
2024-08-19 11:41:58 +0800

    1111

--- app/src/main/java/com/zxd/jitpacksample/MainActivity.kt
+++ app/src/main/java/com/zxd/jitpacksample/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.zxd.jitpacksample
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
-import com.zxd.composetest.ui.NestCollapsingToolbarLayout
-import kotlin.math.abs
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,27 +13,33 @@ class MainActivity : AppCompatActivity() {
 
     private var toolBar: Toolbar? = null
 
+    private var collapsingToolbarLayout: CollapsingToolbarLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.zxd_layout2)
-//        appBarLayout = findViewById(R.id.app_bar)
-//        toolBar = findViewById(R.id.tool_bar)
-//        appBarLayout?.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
-//            val totalScrollRange = appBarLayout.totalScrollRange
-//            val progress = (-verticalOffset).toFloat() / totalScrollRange.toFloat()
+        setContentView(R.layout.zxd_layout)
+        appBarLayout = findViewById(R.id.app_bar)
+        toolBar = findViewById(R.id.tool_bar)
+        toolBar?.setOnClickListener {
+            appBarLayout?.setExpanded(true, true)
+        }
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar)
+
+        appBarLayout?.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val progress = (-verticalOffset).toFloat() / appBarLayout.totalScrollRange.toFloat()
+            toolBar?.alpha = progress
 //            if (abs(verticalOffset) == appBarLayout.totalScrollRange) {
 //                Log.d("@@#", "CollapsingToolbarLayout 完全折叠，显示 Toolbar")
-//                // CollapsingToolbarLayout 完全折叠，显示 Toolbar
 //                toolBar?.visibility = View.VISIBLE
+//                toolBar?.alpha = progress
+//                collapsingToolbarLayout?.alpha = 0f
 //            } else {
 //                Log.d("@@#", "CollapsingToolbarLayout 没有完全折叠，隐藏 Toolbar")
-//                //
 //                toolBar?.visibility = View.GONE
+//                toolBar?.alpha = progress
+//                collapsingToolbarLayout?.alpha = 1 - progress
 //            }
-//            Log.d("@@#", "verticalOffset: $verticalOffset")
-//            Log.d("@@#", "totalScrollRange: $totalScrollRange")
 //            Log.d("@@#", "progress: $progress")
-//
-//        }
+        }
     }
 }
